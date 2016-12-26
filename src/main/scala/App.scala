6960dc034b78da9a02be953acb5283f58db973a4
Andrius Dapševičius <dapsevicius.andrius@gmail.com>
2016-12-26 18:15:45 +0200

    formatting

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -5,48 +5,29 @@ case class Tweet(lang: String, text: String)
 
 object App {
 
-  def main(args: Array[String]): Unit = {
-    withSpark {
-      session =>
-        import session.implicits._
+  def main(args: Array[String]): Unit = sparkSession {
+    session =>
+      import session.implicits._
 
-        val tweetSource = session.read.json("tweets.json").toDF()
+      val availableTweets = session.read.json("tweets.json").toDF()
+        .filter($"lang".isNotNull && $"text".isNotNull && $"lang" =!= "und")
+        .select($"lang", $"text").as[Tweet]
 
-        val availableTweets = tweetSource.filter(
-          $"lang".isNotNull
-            && $"text".isNotNull
-            && $"lang" =!= "und"
-        ).select(
-          $"lang",
-          $"text"
-        ).as[Tweet]
+      val languages = availableTweets
+          .groupBy($"lang").count.sort($"count".desc)
+          .select($"lang").as[String].take(10)
 
-        val languages =
-          availableTweets
-            .groupBy($"lang")
-            .count
-            .sort($"count".desc)
-            .select($"lang")
-            .as[String]
-            .take(10)
+      println(s"Languages we're working with: [${languages.mkString(", ")}]")
+      println()
 
-        println(s"Languages we're working with: [${languages.mkString(", ")}]")
-        println()
-
-        val tweetsForProcessing = availableTweets.filter($"lang".isin(languages: _*))
-        val filteredCount = tweetsForProcessing.count()
-
-        println(s"Selected $filteredCount tweets")
-    }
+      val tweetsForProcessing = availableTweets.filter($"lang".isin(languages: _*))
+      println(s"Selected ${tweetsForProcessing.count()} tweets")
   }
 
-  def withSpark(f: SparkSession => Unit) {
+  def sparkSession(f: SparkSession => Unit) {
     val session = SparkSession.builder().getOrCreate()
     Logger.getRootLogger.setLevel(Level.OFF)
-
     f(session)
-
     session.stop()
   }
-
 }
