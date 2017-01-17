faa8b9e78b22e397d29295cf8b8bfc8d146d18b5
Andrius Dapševičius <dapsevicius.andrius@gmail.com>
2017-01-17 15:14:18 +0200

    realtime tweet clustering

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,5 +1,9 @@
 import SparkUtils._
-import org.apache.spark.ml.{Pipeline, PipelineModel}
+import org.apache.spark.ml.PipelineModel
+import org.apache.spark.ml.linalg.DenseVector
+import org.apache.spark.mllib.clustering.StreamingKMeans
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.rdd.RDD
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
 object App {
@@ -9,16 +13,46 @@ object App {
 
       val ssc = new StreamingContext(session.sparkContext, Seconds(3))
 
-      val receiver = new TwitterReceiver("windows")
+      val receiver = new TwitterReceiver("puppy,giraffe,windows")
       val tweets = ssc.receiverStream(receiver)
 
       val pipeline = PipelineModel.read.load("model")
-      tweets.foreachRDD {
-        rdd =>
-          val f = pipeline.transform(rdd.toDS())
 
-          f.map(_.mkString).foreach(s => println(s))
+      val model = new StreamingKMeans()
+        .setK(3)
+        .setDecayFactor(1.0)
+        .setRandomCenters(8, 0.0)
+
+
+      val factorisedTweets = tweets.transform {
+        rdd =>
+          pipeline.transform(rdd.toDS()).rdd
       }
+
+      val features = factorisedTweets.transform(
+        rdd =>
+          rdd.map {
+            row =>
+              val featuresIndex = row.fieldIndex("features")
+              val dense = row.getAs[DenseVector](featuresIndex)
+              Vectors.dense(dense.values)
+          }
+      )
+
+      val texts = factorisedTweets.transform(
+        rdd =>
+          rdd.map {
+            row => row.getAs[String]("value")
+          }
+      )
+
+      model.trainOn(features)
+
+      val clusters = model.predictOn(features)
+
+      texts.transformWith(clusters, (c: RDD[String], d: RDD[Int]) => {
+        d.zip(c)
+      }).print()
 
       ssc.start()
       ssc.awaitTermination()
